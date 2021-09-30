cbf469cfbd414f6666d6e313f1e7a2be251b8ded
Erick Castille <ecastille924@gmail.com>
2021-09-30 14:37:43 -0400

    writing out post request for fan submit form

--- pl_client/src/actions/SuperfansActions.js
+++ pl_client/src/actions/SuperfansActions.js
@@ -5,3 +5,16 @@ export const fetchSuperfans= () => {
         .then(superfans => dispatch({type: 'FETCH_SUPERFANS', payload: superfans}))
     }
 }
+
+export const postSuperfan = superfan => {
+    return (dispatch) => {
+        fetch('http://localhost:3000/superfans', {
+            method: 'POST',
+            body: JSON.stringify(superfan),
+            headers: { 'Content-type': 'application/json'}
+        })
+        .then(resp => resp.json())
+        .then(superfan => dispatch({type: "POST_SUPERFAN", payload: superfan}))
+    
+    }
+}
