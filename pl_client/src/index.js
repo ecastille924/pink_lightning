76df6f3fbdfcb79cdcfded079f0301784944f706
Erick Castille <ecastille924@gmail.com>
2021-09-30 13:26:04 -0400

    installing react devtools extension

--- pl_client/src/index.js
+++ pl_client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux'
