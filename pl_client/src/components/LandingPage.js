459b9351cdd147b99ebc5e8f9fde358e1f179f0b
Erick Castille <ecastille924@gmail.com>
2021-10-01 17:33:19 -0400

    adding booking and isntagram info with link to landing page

--- pl_client/src/components/LandingPage.js
+++ pl_client/src/components/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import '../App.css'
 
 export default function LandingPage() {
     return (
             <div className="landing">
                 <img src= '../images/pl_logo.jpg' alt="" className="landing-image"/>
                 <p className="landing-tag">NYC's premier rock and roll cover band</p>
+                <p className="landing-sub-tag">booking - pinklightningnyc@gmail.com</p>
+                <p><img className="ig-icon" src= '../images/ig_icon.png' alt=""/>"""<a className="landing-sub-tag" href='https://www.instagram.com/pinklightningnyc/?hl=en'>@pinkligntningnyc</a> </p>
             </div>
         
     )
