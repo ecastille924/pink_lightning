21a44fb782400aea7a597fba5df6e54690409c94
Erick Castille <ecastille924@gmail.com>
2021-10-04 17:30:41 -0400

    removing unused code and achanging background color for lists

--- pl_client/src/components/SuperfansList.js
+++ pl_client/src/components/SuperfansList.js
@@ -8,9 +8,6 @@ function SuperfansList({superfans}) {
         <div className="show-div">
             <p className = "landing-sub-tag">Thank you to our most recent superfans!</p>
             <p className = "landing-sub-tag">See you at the show, </p>
-            {/* <h2 className = "superfan-name">{superfans.reduce((i, superfan) => 
-                i = i > superfan.id ? i : (superfan.firstname), 0)}!</h2> */}
-                
                 {superfans.map(superfan =>
                      <ul key={superfan.id} className= "ul"> <li key={superfan.firstname} className= "superfan-name">
                          {superfan.firstname} <br/>
